55381bc08d89fb2fcdc15822d768cae3590cdb64
Blake Hannaford <blake.hannaford@gmail.com>
2025-12-04 15:58:14 -0800

    reverted LCD_task

--- main/LCD_task.c
+++ main/LCD_task.c
@@ -34,75 +34,53 @@ extern void handle_error(char* msg);
 #ifdef  CONFIG_DRIVE_LCD_YES
 #define TAG  "DRIVE LCD_16x2_task"
 
-void LCD_reset(int lcd_addr) {
-    int lcd = lcd_addr;
-    if (xSemaphoreTake(i2cMutex, portMAX_DELAY) == pdTRUE) {
-        printf("Starting: 0x%x\n", lcd);
-        
-        lcd_init(lcd);             // Initialize the LCD
-        usleep(d100ms);
+char lcd_LOG_message[] = "CONFIG:DRIVE_LCD:  YES_LCD, We will be using LCD via i2c";
 
-        lcd_clear(lcd);            // Clear the LCD screen
-        usleep(d100ms);
+esp_err_t  LCD_16x2_init() {
+    lcd_init();             // Initialize the LCD
+    usleep(d100ms);
 
- 
-        usleep(d100ms);
-        xSemaphoreGive(i2cMutex);
-        usleep(d100ms);
-        vTaskDelay(pdMS_TO_TICKS(1000));
-    }
+    lcd_clear();            // Clear the LCD screen
+    usleep(d100ms);
+
+    lcd_put_cursor(0, 0);   // Set cursor position to   row, column
+    usleep(d100ms);
+    lcd_send_string("lcd_init()2 ...");
+    usleep(d100ms);
+    return((esp_err_t) 0);   // no error checks are implemented yet
 }
 
-void LCD_task1(void *lcd_addr) {
+void LCD_16x2_task(void*){
     int i=0;   // iteration counter
     char numst[20];  // place to hold string to print
-    
-    int lcd = (uint32_t)lcd_addr;
+    while (1){
+        ESP_LOGI(TAG,"  LCD TASK IS ACTIVE");
+        if (i2cMutex != NULL){
+            if( xSemaphoreTake( i2cMutex, ( TickType_t ) 10 ) == pdTRUE )
+            {
+                /* We were able to obtain the semaphore and can now access the
+                shared resource. */
 
-    char buffer[20];
-    lcd_put_cursor(lcd, 0, 0);   // Set cursor position to   row, column
-    sprintf(buffer, "Works: 0x%x\n", lcd);
-    lcd_send_string(lcd, buffer);
+                lcd_put_cursor(1, 0);
 
-    while (1) {
-        if (xSemaphoreTake(i2cMutex, portMAX_DELAY) == pdTRUE) {
-            /* We were able to obtain the semaphore and can now access the
-//                 shared resource. */
-            lcd_put_cursor(lcd, 1, 0);
+                sprintf(numst, "N: %04d",i++);
+                lcd_send_string(numst);     // Display the count (numst)
+                //usleep(3*d100ms);
 
-            sprintf(numst, "N: %04d", i++);
-
-            lcd_send_string(lcd, numst);     // Display the count (numst)
-            //usleep(3*d100ms);
-
-            /* We have finished accessing the shared resource. Release the
-            semaphore. */
-            xSemaphoreGive(i2cMutex);
-            vTaskDelay(pdMS_TO_TICKS(1000));
+                /* We have finished accessing the shared resource. Release the
+                semaphore. */
+                xSemaphoreGive( i2cMutex );
+            }
+            else handle_error("lcd_init Mutex Timeout");
         }
-    }
-}
+        else handle_error("i2c Mutex FAIL");
 
-void LCD_task2(void *lcd_addr) {
-    int lcd = (uint32_t)lcd_addr;
-    char buffer[20];
-    
-    sprintf(buffer, "LCD 0x%x works!", lcd);
-
-    uint8_t flip = 0;
-
-    while (1) {
-        if(xSemaphoreTake(i2cMutex, portMAX_DELAY) == pdTRUE) {
-            lcd_clear(lcd);
-            lcd_put_cursor(lcd, flip, 0);
-            
-            lcd_send_string(lcd, buffer);
+        ESP_LOGI(TAG, "LCD update sent");
+        vTaskDelay(1000/portTICK_PERIOD_MS);
         }
-        xSemaphoreGive(i2cMutex);
-        flip = flip ^ 1;
-        vTaskDelay(pdMS_TO_TICKS(1000)); 
-    }
-}
+
+
+     }
 
 #elif   CONFIG_DRIVE_LCD_NO
 #define TAG  "NO LCD_16x2_task"
@@ -111,14 +89,15 @@ char lcd_LOG_message[] = "CONFIG:DRIVE_LCD:  NO_LCD ";
 
 void LCD_16x2_task(void*){
     while(1){
-        ESP_LOGI(TAG, "placeholder: LCD_16x2_task()");
+        ESP_LOGI(TAG, "LCD not configed: placeholder: LCD_16x2_task()");
         vTaskDelay(2000/portTICK_PERIOD_MS);
     }
 }
 
 
-void LCD_16x2_init(void) {
-    ESP_LOGI(TAG, "placeholder: LCD_16x2_init()");
+esp_err_t LCD_16x2_init() {
+    ESP_LOGI(TAG, "LCD not configed: placeholder: LCD_16x2_init()");
+    return((esp_err_t) 1);  // conf for no LCD is not an error but we can ID it
 }
 #else
 #error "unsupported LCD config"
